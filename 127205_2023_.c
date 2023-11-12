79c594bd671a38307bc68d00948862c78ef4f387
patrikkubacka <patrikkubacka333@gmail.com>
2023-11-12 17:07:39 +0100

    skoro z a spravene c

--- 127205_2023_.c
+++ 127205_2023_.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void v(FILE **f, int pressed_n, int *pressed_v, char **ID_mer_mod, char **Poz_mod, char **Typ_mer_vel, double *Hodnota, char **Cas_mer, char **Dat_mer)
+void v(FILE **f, int pressed_n, int *pressed_v, char **ID_mer_mod, char **Poz_mod, char **Typ_mer_vel, double *Hodnota, char **Cas_mer, char **Dat_mer, int number)
 {
     char line[300];
     *f = fopen("dataloger.txt", "r");
@@ -51,10 +51,11 @@ void v(FILE **f, int pressed_n, int *pressed_v, char **ID_mer_mod, char **Poz_mo
                 printf("%s", line);
                 counter++;
             }
+            fseek(*f, 0, SEEK_SET);
         }
         if (pressed_n == 1) // ak bolo stlacene "n" tak vypise zaznamy z dyn. polí
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < number; i++)
             {
                 printf("Id mer. modulu: ");
                 printf("%s\n", ID_mer_mod[i]);
@@ -66,14 +67,16 @@ void v(FILE **f, int pressed_n, int *pressed_v, char **ID_mer_mod, char **Poz_mo
                 printf("%g\n", Hodnota[i]);
                 printf("Cas merania: ");
                 printf("%s\n", Cas_mer[i]);
+                printf("Datum merania: ");
+                printf("%s\n", Dat_mer[i]);
                 printf("\n");
             }
         }
     }
-    fseek(*f, 0, SEEK_SET);
+    printf("\n");
 }
 
-void n(FILE *f, int *pressed_n, int pressed_v, char ***ID_mer_mod, char ***Poz_mod, char ***Typ_mer_vel, double **Hodnota, char ***Cas_mer, char ***Dat_mer)
+void n(FILE *f, int *pressed_n, int pressed_v, char ***ID_mer_mod, char ***Poz_mod, char ***Typ_mer_vel, double **Hodnota, char ***Cas_mer, char ***Dat_mer, int *number)
 {
     if (pressed_v == 0) // ak nebolo stlacene v tak subor nebol otvoreny
     {
@@ -86,6 +89,22 @@ void n(FILE *f, int *pressed_n, int pressed_v, char ***ID_mer_mod, char ***Poz_m
         char line[100];
         int counter = 1;
         fseek(f, 0, SEEK_SET);
+        if (*pressed_n == 1)
+        {
+            free(*ID_mer_mod);
+            *ID_mer_mod = NULL;
+            free(*Poz_mod);
+            *Poz_mod = NULL;
+            free(*Typ_mer_vel);
+            *Typ_mer_vel = NULL;
+            free(*Hodnota);
+            *Hodnota = NULL;
+            free(*Cas_mer);
+            *Cas_mer = NULL;
+            free(*Dat_mer);
+            *Dat_mer = NULL;
+        }
+
         while (fgets(line, sizeof(line), f) != NULL) // ráta počet záznamov v subore dataloger
         {
             if (line[0] == '\n')
@@ -93,6 +112,7 @@ void n(FILE *f, int *pressed_n, int pressed_v, char ***ID_mer_mod, char ***Poz_m
                 counter++;
             }
         }
+
         *ID_mer_mod = (char **)malloc(counter * sizeof(char *));
         *Poz_mod = (char **)malloc(counter * sizeof(char *));
         *Typ_mer_vel = (char **)malloc(counter * sizeof(char *));
@@ -103,49 +123,252 @@ void n(FILE *f, int *pressed_n, int pressed_v, char ***ID_mer_mod, char ***Poz_m
         char row[100];
         double cislo;
         fseek(f, 0, SEEK_SET);
+
         for (int i = 0; i < counter; i++)
         {
+
             fgets(row, sizeof(row), f); // nacita prvy riadok suboru a ulozi do row
             (*ID_mer_mod)[i] = (char *)malloc(6 * sizeof(char));
-            row[5] = '\0';
+            row[5] = '\0';                           // za posledny znak v row da prazdny znak
             (char *)strcpy(((*ID_mer_mod)[i]), row); // skopiruje obsah row do dyn. pola
 
             fgets(row, sizeof(row), f); // nacita druhy riadok zo suboru a ulozi do row
             (*Poz_mod)[i] = (char *)malloc(15 * sizeof(char));
-            row[14] = '\0';
+            row[14] = '\0';                     // za posledny znak v row da prazdny znak
             (char *)strcpy((*Poz_mod)[i], row); // skopiruje obsah row do dyn. pola
 
             fgets(row, sizeof(row), f); // nacita treti riadok zo suboru a ulozi do row
             (*Typ_mer_vel)[i] = (char *)malloc(3 * sizeof(char));
-            row[2] = '\0';
+            row[2] = '\0';                          // za posledny znak v row da prazdny znak
             (char *)strcpy((*Typ_mer_vel)[i], row); // skopiruje obsah row do dyn. pola
 
             fgets(row, sizeof(row), f); // nacita stvrty riadok zo suboru a ulozi do row
-            cislo = atof(row);
-            (*Hodnota)[i] = cislo; // skopiruje obsah row do dyn. pola
+            cislo = atof(row);          // konvertuje char na double
+            (*Hodnota)[i] = cislo;      // skopiruje obsah row do dyn. pola
 
             fgets(row, sizeof(row), f); // nacita piaty riadok zo suboru a ulozi do row
             (*Cas_mer)[i] = (char *)malloc(5 * sizeof(char));
-            row[4] = '\0';
+            row[4] = '\0';                      // za posledny znak v row da prazdny znak
             (char *)strcpy((*Cas_mer)[i], row); // skopiruje obsah row do dyn. pola
-            (*Cas_mer)[i][4] = '\0';
 
             fgets(row, sizeof(row), f); // nacita siesty riadok zo suboru a ulozi do row
             (*Dat_mer)[i] = (char *)malloc(9 * sizeof(char));
-            row[8] = '\0';
+            row[8] = '\0';                      // za posledny znak v row da prazdny znak
             (char *)strcpy((*Dat_mer)[i], row); // skopiruje obsah row do dyn. pola
 
-            fgets(row, sizeof(row), f);
+            fgets(row, sizeof(row), f); // nacita prazdny riadok za blokom zaznamu
         }
         *pressed_n = 1;
+        *number = counter;
+    }
+    printf("\n");
+}
+
+void c(char **ID_mer_mod, int number, char **Dat_mer, int pressed_n)
+{
+    int y;
+    char ID[50];
+    char line[50];
+    char Date[50];
+    int count = 1;
+    int was_ciach = 0;
+    int correct = 1;
+
+    if (pressed_n == 0)
+    {
+        printf("Polia nie su vytvorene.\n");
+        return;
+    }
+
+    else
+    {
+        scanf("%d", &y); // nacita pocet mesiacov zo vstupu
+        FILE *file = fopen("ciachovanie.txt", "r");
+        while (fgets(line, sizeof(line), file) != NULL) // ráta počet záznamov v subore ciachovanie
+        {
+            if (line[0] == '\n')
+            {
+                count++;
+            }
+        }
+        fseek(file, 0, SEEK_SET);
+
+        for (int j = 0; j < number; j++)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                fgets(ID, sizeof(ID), file);
+                ID[5] = '\0';
+                fgets(Date, sizeof(Date), file);
+                Date[8] = '\0';
+                if (strcmp(ID, ID_mer_mod[j]) == 0)
+                {
+                    was_ciach++;
+                    int mes_ciach = ((atol(Date) / 100) % 100); // atol konvertuje string na long
+                    int mes_datalog = ((atol(Dat_mer[j]) / 100) % 100);
+                    int after_ciach = mes_datalog - mes_ciach;
+                    if (after_ciach > 0 && after_ciach > y)
+                    {
+                        printf("%s je %d mesiacov po ciachovani.\n", ID, (after_ciach - y));
+                        correct = 0;
+                    }
+                }
+                fgets(ID, sizeof(ID), file);
+            }
+            if (was_ciach == 0)
+            {
+                printf("%s nie je ciachovaný.\n", ID_mer_mod[j]);
+                correct = 0;
+            }
+            was_ciach = 0;
+            fseek(file, 0, SEEK_SET);
+        }
+        if (correct == 1)
+        {
+            printf("Data su korektné.");
+        }
     }
 }
 
-void c()
+int compare(char *first_c, char *second_c)
 {
-    int y;
-    scanf("%d", &y); // nacita pocet mesiacov zo vstupu
-    FILE *file = fopen("ciachovanie.txt", "r");
+    while (*first_c != '\0' && *second_c != '\0')
+    {
+        if (*first_c != *second_c)
+        {
+            return 0;
+        }
+        first_c++;
+        second_c++;
+    }
+    if (*first_c == '\0' && *second_c == '\0')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void s(char **ID_mer_mod, char **Typ_mer_vel, int number, int pressed_n)
+{
+    char ID[6];
+    char TYP[3];
+    int c = 0;
+    scanf("%s %s", ID, TYP);
+
+    if (pressed_n == 0)
+    {
+        printf("polia nie su vytvorene.\n");
+        return;
+    }
+
+    else
+    {
+        for (int i = 0; i < number; i++) // ráta pocet zaznamov v subore rovnakych so vstupom
+        {
+            if (compare(ID_mer_mod[i], ID) == 1)
+            {
+                if (compare(Typ_mer_vel[i], TYP) == 1)
+                {
+                    c += 1;
+                }
+            }
+        }
+
+        if (c == 0)
+        { // ak nie su ziadne rovnake zaznamy ako vstup tak vypise ze neexistuju
+            printf("Pre dany vstup neexistuju zaznamy.");
+            return;
+        }
+    }
+}
+
+void h(int pressed_n, char **Typ_mer_vel, int number)
+{
+    if (pressed_n == 0)
+    {
+        printf("Polia nie su vytvorene.\n");
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < number; i++)
+        {
+            compare(Typ_mer_vel[i], "RM") == 1;
+        }
+    }
+}
+
+void z(int pressed_n, int *number, char ***ID_mer_mod, char ***Poz_mod, char ***Typ_mer_vel, double **Hodnota, char ***Cas_mer, char ***Dat_mer)
+{
+    char ID[6];
+    int poc = 0;
+
+    if (pressed_n == 0)
+    {
+        printf("Polia nie su vytvorene.\n");
+        return;
+    }
+
+    else
+    {
+        double *temp_hodnota = (double *)malloc((*number) * sizeof(double));
+
+        for (int i = 0; i < *number; i++)
+        {
+            temp_hodnota[i] = (*Hodnota)[i];
+        }
+        free(*Hodnota);
+
+        scanf("%s", ID);
+        for (int i = 0; i < *number; i++)
+        {
+            if (compare((*ID_mer_mod)[i], ID) == 1)
+            {
+                free((*ID_mer_mod)[i]);
+                (*ID_mer_mod)[i] = NULL;
+                free((*Poz_mod)[i]);
+                (*Poz_mod)[i] = NULL;
+                free((*Typ_mer_vel)[i]);
+                (*Typ_mer_vel)[i] = NULL;
+                free((*Cas_mer)[i]);
+                (*Cas_mer)[i] = NULL;
+                free((*Dat_mer)[i]);
+                (*Dat_mer)[i] = NULL;
+                poc++;
+            }
+        }
+        *Hodnota = (double *)malloc(((*number) - poc) * sizeof(double));
+        int x = 0;
+        for (int i = 0; i < *number; i++)
+        {
+            if ((*ID_mer_mod)[i] != NULL)
+            {
+                (*ID_mer_mod)[x] = (*ID_mer_mod)[i];
+                (*Poz_mod)[x] = (*Poz_mod)[i];
+                (*Typ_mer_vel)[x] = (*Typ_mer_vel)[i];
+                (*Hodnota)[x] = (temp_hodnota)[i];
+                (*Cas_mer)[x] = (*Cas_mer)[i];
+                (*Dat_mer)[x] = (*Dat_mer)[i];
+                x++;
+            }
+        }
+        printf("Vymazalo sa: %d zaznamov!\n", poc);
+
+        for (int i = (*number - poc); i < *number; i++)
+        {
+            free((*ID_mer_mod)[i]);
+            (*ID_mer_mod)[i] = NULL;
+            free((*Poz_mod)[i]);
+            (*Poz_mod)[i] = NULL;
+            free((*Typ_mer_vel)[i]);
+            (*Typ_mer_vel)[i] = NULL;
+            free((*Cas_mer)[i]);
+            (*Cas_mer)[i] = NULL;
+            free((*Dat_mer)[i]);
+            (*Dat_mer)[i] = NULL;
+        }
+        *number = *number - poc;
+    }
 }
 
 int main(void)
@@ -155,31 +378,37 @@ int main(void)
     int pressed_n = 0;
     char **ID_mer_mod = NULL, **Poz_mod = NULL, **Typ_mer_vel = NULL, **Cas_mer = NULL, **Dat_mer = NULL;
     double *Hodnota = NULL;
+    int number = 0;
     char letter;
     do
     {
         scanf("%c", &letter); // nacita pismeno zo vstupu
-        switch (letter)       
+        switch (letter)
         {
         case 'v':
-            v(&f, pressed_n, &pressed_v, ID_mer_mod, Poz_mod, Typ_mer_vel, Hodnota, Cas_mer, Dat_mer); //pošle parametra funkcii 'v' a vyvola ju
+            v(&f, pressed_n, &pressed_v, ID_mer_mod, Poz_mod, Typ_mer_vel, Hodnota, Cas_mer, Dat_mer, number); // pošle parametra funkcii 'v' a vyvola ju
             break;
 
         case 'n':
-            n(f, &pressed_n, pressed_v, &ID_mer_mod, &Poz_mod, &Typ_mer_vel, &Hodnota, &Cas_mer, &Dat_mer); //pošle parametra funkcii 'n' a vyvola ju
+            n(f, &pressed_n, pressed_v, &ID_mer_mod, &Poz_mod, &Typ_mer_vel, &Hodnota, &Cas_mer, &Dat_mer, &number); // pošle parametra funkcii 'n' a vyvola ju
             break;
 
         case 'c':
-            c();
+            c(ID_mer_mod, number, Dat_mer, pressed_n);
             break;
-            /*
+
         case 's':
-            s();
+            s(ID_mer_mod, Typ_mer_vel, number, pressed_n);
             break;
+
         case 'h':
-            h();
+            h(pressed_n, Typ_mer_vel, number);
             break;
-            */
+
+        case 'z':
+            z(pressed_n, &number, &ID_mer_mod, &Poz_mod, &Typ_mer_vel, &Hodnota, &Cas_mer, &Dat_mer);
+            break;
+
         case 'k':
             free(ID_mer_mod);
             ID_mer_mod = NULL;
